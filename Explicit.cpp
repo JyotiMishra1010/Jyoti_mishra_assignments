3c722f7f74112e22365368f7235ad54295ec9c7d
Jyoti Mishra <113105374+JyotiMishra1010@users.noreply.github.com>
2025-02-27 14:56:19 +0530

    Explicit.cpp

---
+++ Explicit.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class A {
+public:
+	//implicit conversion 
+	A(int x)
+	{
+		cout << "this default constructor"<<x<<endl;
+	}
+	// explicit paramterized constructor
+	explicit A(int a, int b) {
+		cout << "this is explicit key word use case"<<a<<" "<<b<<endl;
+	}
+	//explicit copy constructor
+	explicit A( const A& object)
+	{
+		cout << "this explicit copy constructor ";
+
+	}
+};
+int main()
+{
+	A obj = 10;
+	A obj2(19, 20);
+	A obj3(obj);
+	return 0;
+}
