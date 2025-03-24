adbee1979f82f42a6d6366d5454ab3815a6cfda8
Jyoti Mishra <113105374+JyotiMishra1010@users.noreply.github.com>
2025-03-24 15:50:18 +0530

    shared_ptr.cpp

--- shared_ptr.cpp
+++ shared_ptr.cpp
@@ -11,18 +11,30 @@ public:
 	~myclass() {
 		std::cout << "this is destructor called" << std::endl;
 	}
+	void show() {
+		std::cout << "this function is in class" << std::endl;
+	}
 
 
 };
 int main() {
 	std::shared_ptr<myclass>ptr1 = std::make_shared<myclass>();
-	std::cout << ptr1.use_count()<<std::endl;//1
+	//through get
+	myclass* rawptr = ptr1.get();
+	rawptr->show();
+
+
+	std::cout << ptr1.use_count()<<std::endl;
 	std::shared_ptr<myclass>ptr2 = ptr1;
-	std::cout << ptr1.use_count() << std::endl;//2
+	std::cout << ptr1.use_count() << std::endl;
 	std::shared_ptr<myclass>ptr3 = ptr1;
-	std::cout << ptr3.use_count() << std::endl;//3
+	std::cout << ptr3.use_count() << std::endl;
 	ptr1.reset();
-	std::cout << ptr1.use_count() << std::endl;//0
+	std::cout << ptr1.use_count() << std::endl;
+
+
+	
+
 	return 0;
 
 
