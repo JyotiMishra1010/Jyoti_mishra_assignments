d82eda33bd5f8cb9f82dfaea655370bf3e019e53
Jyoti Mishra <113105374+JyotiMishra1010@users.noreply.github.com>
2025-03-26 14:17:57 +0530

    lambda -1.cpp

--- lambda -1.cpp
+++ lambda -1.cpp
@@ -1,1 +1,32 @@
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template <typename Func>
+void apply(Func f, int val) { std::cout << "Result: " << f(val) << std::endl; }
+
+int main() {
+    int x = 10, y = 20;
+
+    auto hello = [] { std::cout << "Hello, from c++!\n"; };
+    auto add = [](int a, int b) { return a + b; };
+    auto Val = [x] { std::cout << "Captured: " << x << std::endl; };
+    auto Ref = [&y] { y += 5; std::cout << "Modified y: " << y << std::endl; };
+    auto outerLambda = [](int factor) { return [factor](int num) { return num * factor; }; };
+    auto square = [](auto n) { return n * n; };
+
+    hello();
+    std::cout << "Sum: " << add(5, 3) << std::endl;
+    Val();
+    Ref();
+    std::cout << "Multiplied: " << outerLambda(3)(4) << std::endl;
+
+    std::vector<int> v = { 5, 2, 9, 1, 6 };
+    std::sort(v.begin(), v.end(), [](int a, int b) { return a < b; });
+    for (int n : v) std::cout << n << " ";
+    std::cout << std::endl;
+
+    apply(square, 5);
+    apply(square, 3.5);
+}
