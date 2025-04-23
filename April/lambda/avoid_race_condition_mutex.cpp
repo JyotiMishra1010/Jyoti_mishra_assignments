fe033b9faacef9caeab5d06e5d7a835a6a6dc85a
Jyoti Mishra <113105374+JyotiMishra1010@users.noreply.github.com>
2025-04-23 09:27:54 +0530

    Create avoid_race_condition_mutex.cpp

---
+++ April/lambda/avoid_race_condition_mutex.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<thread>
+#include<mutex>
+int count = 0;
+std::mutex mtx;
+void increment() {
+	for (int i = 0;i < 500;i++) {
+		std::lock_guard<std::mutex>lock(mtx);
+		count++;
+	}
+}
+int main() {
+	std::thread t1(increment);
+	std::thread t2(increment);
+	t1.join();
+	t2.join();
+	std::cout << "this value of count" << count << std::endl;
+	return 0;
+	
+}
+//it can give the wrong result bcz it's race condition where multiple threads work on change of same common data that is race condition
