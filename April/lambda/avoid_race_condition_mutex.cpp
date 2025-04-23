f4c5410d18fe2341efb05119af13976a744f018c
Jyoti Mishra <113105374+JyotiMishra1010@users.noreply.github.com>
2025-04-23 09:29:52 +0530

    Update avoid_race_condition_mutex.cpp

--- April/lambda/avoid_race_condition_mutex.cpp
+++ April/lambda/avoid_race_condition_mutex.cpp
@@ -8,7 +8,13 @@ void increment() {
 		std::lock_guard<std::mutex>lock(mtx);
 		count++;
 	}
-}
+	//or we can also write
+	/*for (int i = 0;i < 500;i++) {
+		mtx.lock();
+		++count;
+		mtx.unlock();
+	}
+}*/
 int main() {
 	std::thread t1(increment);
 	std::thread t2(increment);
@@ -18,4 +24,3 @@ int main() {
 	return 0;
 	
 }
-//it can give the wrong result bcz it's race condition where multiple threads work on change of same common data that is race condition
